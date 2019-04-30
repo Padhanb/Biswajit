1d8ade107dff5c78445abc15228f4b6a93fa3e20
Biswajit Padhan <biswajitpadhan@gmail.com>
2019-04-30 13:48:24 +0000

    added four function

--- make.py
+++ make.py
@@ -1,3 +1,12 @@
 ## its my first phthon Program ##
 x = 12
 print x
+def add(x,y):
+  return x + y
+def substract(x, y):
+  return x - y
+def multiply(x, y):
+  return x * y
+def divide(x, y):
+  return x / y
+
